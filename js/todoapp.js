28ba866587b7bfbaf986b21ea5d0e96e12bd2dfe
WeiSheng_Cheng <weisheng_cheng@asus.com>
2019-09-04 19:41:11 +0800

    Add arrangement of todo item being done or changing back to ongoing
    BUG: when user don't wait the animation to end, the ui state will be wrong.
    Need to solve this.

--- js/todoapp.js
+++ js/todoapp.js
@@ -82,9 +82,10 @@ $(document).ready(function() {
     }
   });
   class ToDoItem {
-    constructor(todo_content, update_time, is_done) {
+    constructor(todo_content, init_time, is_done) {
       this.todo_content = todo_content;
-      this.update_time = update_time;
+      this.init_time = init_time;
+      this.update_time = init_time;
       this.is_done = is_done;
     }
   }
@@ -117,7 +118,7 @@ $(document).ready(function() {
     }
     return parseInt(y + M + d + h + m + s + ms);
   }
-  todoArr = [
+  var todoArr = [
     new ToDoItem(
       "Eat breakfast",
       formatDateTime(new Date(2000, 0, 1, 0, 0, 1, 0)),
@@ -130,16 +131,22 @@ $(document).ready(function() {
     )
   ];
 
-  function get_new_todo_item_html_div(todoItem, hide_when_add) {
+  function get_todo_item_html_div(todoItem, hide_when_add) {
     todo_item_div =
-      '<div class="row no-gutters todo-item text-left ongoing" id="' +
-      todoItem.update_time +
+      '<div class="row no-gutters todo-item text-left ' +
+      (todoItem.is_done ? "done" : "ongoing") +
+      '" id="' +
+      todoItem.init_time +
       (hide_when_add ? '" style="display: none;' : "") +
       '"> \
   <div class="col-1"> \
-  <input type="checkbox" class="todo-checkbox" /> \
+  <input type="checkbox" class="todo-checkbox" ' +
+      (todoItem.is_done ? "checked" : "") +
+      ' /> \
   </div> \
-  <div class="col-10" contenteditable="true">' +
+  <div class="col-10 todo-content" ' +
+      (todoItem.is_done ? "" : 'contenteditable="true"') +
+      ">" +
       todoItem.todo_content +
       '</div> \
   <div class="col-1"> \
@@ -162,17 +169,16 @@ $(document).ready(function() {
       // if todo section now
       switch (current_todo_id) {
         case TODO_ID:
-          $("#todo-items").prepend(get_new_todo_item_html_div(todoItem, true));
-          $("#" + todoItem.update_time).show("slow");
-          $("#all-items").prepend(get_new_todo_item_html_div(todoItem, false));
+          $("#todo-items").prepend(get_todo_item_html_div(todoItem, true));
+          $("#" + todoItem.init_time).show("slow");
+          $("#all-items").prepend(get_todo_item_html_div(todoItem, false));
           break;
         case ALL_ID:
-          $("#all-items").prepend(get_new_todo_item_html_div(todoItem, true));
-          $("#" + todoItem.update_time).show("slow");
-          $("#todo-items").prepend(get_new_todo_item_html_div(todoItem, false));
+          $("#all-items").prepend(get_todo_item_html_div(todoItem, true));
+          $("#" + todoItem.init_time).show("slow");
+          $("#todo-items").prepend(get_todo_item_html_div(todoItem, false));
           break;
       }
-
       $("#todo-input").val("");
       return true;
     } else {
@@ -210,26 +216,144 @@ $(document).ready(function() {
     var todoItemText = $(this)
       .parent()
       .next();
+    var todoItemBar = $(this)
+      .parent()
+      .parent();
 
+    // don't let user click the checkbox when arrangement of all
+    // section is not done yet
     todo_item_checkbox.prop("disabled", true);
-    if (todoItemText.attr("class").indexOf("done") >= 0) {
-      todoItemText.removeClass("done");
+
+    if (todoItemBar.attr("class").indexOf("done") >= 0) {
+      // the TODO item set to undone
+      var ongoing_todo_item;
+      todoArr.forEach(function(todo_item) {
+        if (todo_item_id == todo_item.init_time) {
+          todo_item.update_time = formatDateTime(new Date());
+          todo_item.is_done = false;
+          ongoing_todo_item = todo_item;
+        }
+      });
+      todoItemBar.removeClass("done");
+      todoItemBar.addClass("ongoing");
       todoItemText.addClass("remove-strikethrough");
       todoItemText.one("animationend", function(e) {
         todoItemText.removeClass("remove-strikethrough");
-        todo_item_checkbox.prop("disabled", false);
+        switch (current_todo_id) {
+          case TODO_ID:
+            // should never happen
+            break;
+          case ALL_ID:
+            add_ongoing_item_to_todo_section(ongoing_todo_item);
+            todoItemBar.fadeOut(300, function() {
+              todoItemBar.remove();
+            });
+            // move it to top
+            add_done_item_to_all_section(done_todo_item, true);
+            $("#done-items #" + ongoing_todo_item.init_time).remove();
+            break;
+          case DONE_ID:
+            todoItemBar.fadeOut(300, function() {
+              todoItemBar.remove(false);
+            });
+            $("#all-items #" + ongoing_todo_item.init_time).remove();
+            add_ongoing_item_to_todo_section(ongoing_todo_item);
+            add_ongoing_item_to_all_section(ongoing_todo_item, false);
+            break;
+        }
       });
     } else {
+      // the TODO item set to done
+      var done_todo_item;
+      todoArr.forEach(function(todo_item) {
+        if (todo_item_id == todo_item.init_time) {
+          todo_item.update_time = formatDateTime(new Date());
+          todo_item.is_done = true;
+          done_todo_item = todo_item;
+        }
+      });
+      console.log("donetodo=", done_todo_item);
       todoItemText.removeClass("remove-strikethrough");
-      todoItemText.addClass("done");
+      todoItemBar.removeClass("ongoing");
+      todoItemBar.addClass("done");
       todoItemText.addClass("do-strikethrough");
       todoItemText.one("animationend", function(e) {
         // console.log("animation name=", e.originalEvent.animationName);
         todoItemText.removeClass("do-strikethrough");
-        todo_item_checkbox.prop("disabled", false);
+        switch (current_todo_id) {
+          case TODO_ID:
+            todoItemBar.fadeOut(300, function() {
+              todoItemBar.remove(false);
+            });
+            $("#all-items #" + done_todo_item.init_time).remove();
+            add_done_item_to_all_section(done_todo_item, false);
+            break;
+          case ALL_ID:
+            $("#todo-items #" + done_todo_item.init_time).remove();
+            todoItemBar.fadeOut(300, function() {
+              todoItemBar.remove();
+            });
+            // move it behind ongoing items
+            add_done_item_to_all_section(done_todo_item, true);
+            break;
+          case DONE_ID:
+            // should never happen
+            break;
+        }
       });
+      add_done_item_to_done_section(done_todo_item);
     }
-    //todo_item_checkbox.on("click", todo_checkbox_onclick);
   });
+  function add_done_item_to_all_section(done_todo_item, need_animation) {
+    if (need_animation) {
+      // move to top of done items
+      if ($("#all-items>div.ongoing").length > 0) {
+        // 1.find the ongoing end
+        $("#all-items>div.ongoing")
+          .last()
+          .after(get_todo_item_html_div(done_todo_item, true));
+      } else if ($("#all-items>div.done".length > 0)) {
+        // 2.if no ongoing items, find 1st done item
+        $("#all-items>div.done")
+          .first()
+          .before(get_todo_item_html_div(done_todo_item, true));
+      } else {
+        // 3.if there is no ongoing, done items do nothing
+        $("#all-items").append(get_todo_item_html_div(done_todo_item, true));
+      }
+      $("#all-items #" + done_todo_item.init_time).show("slow");
+    } else {
+      // move to top of done items
+      if ($("#all-items>div.ongoing").length > 0) {
+        // 1.find the ongoing end
+        $("#all-items>div.ongoing")
+          .last()
+          .after(get_todo_item_html_div(done_todo_item, false));
+      } else if ($("done".length > 0)) {
+        // 2.if no ongoing items, find 1st done item
+        $("#all-items>div.done")
+          .first()
+          .before(get_todo_item_html_div(done_todo_item, false));
+      } else {
+        // 3.if there is no ongoing, done items do nothing
+        $("#all-items").append(get_todo_item_html_div(done_todo_item, false));
+      }
+      $("#all-items #" + done_todo_item.init_time).prop("checked", true);
+    }
+  }
+  function add_done_item_to_done_section(done_todo_item) {
+    $("#done-items").prepend(get_todo_item_html_div(done_todo_item, false));
+  }
+  function add_ongoing_item_to_all_section(ongoing_todo_item, need_animation) {
+    if (need_animation) {
+      $("#all-items").prepend(get_todo_item_html_div(ongoing_todo_item, true));
+      $("#all-items #" + done_todo_item.init_time).show("slow");
+    } else {
+      $("#all-items").prepend(get_todo_item_html_div(ongoing_todo_item, false));
+    }
+  }
+  function add_ongoing_item_to_todo_section(done_todo_item) {
+    $("#todo-items").prepend(get_todo_item_html_div(done_todo_item, false));
+  }
   // Initialize demo TODO items
 });
